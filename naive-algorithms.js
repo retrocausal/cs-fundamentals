db1226118762f937b27ab0505376925aeaee8384
Nitin Prabhakar <pmitwithnitin@gmail.com>
2018-05-22 15:02:26 +0530

    Add Independant set algorithm

--- naive-algorithms.js
+++ naive-algorithms.js
@@ -20,8 +20,8 @@ const edgesToCover = network.reduce( ( edges, vertex ) => {
   return edges;
 }, new Set() );
 /*
-*@vertexCover checks if the given combination is viable and covers all edges
-*/
+ *@vertexCover checks if the given combination is viable and covers all edges
+ */
 const vertexCover = function ( combinations, combination ) {
   //helper for minimum vertex cover
   let sum = 0;
@@ -35,7 +35,7 @@ const vertexCover = function ( combinations, combination ) {
     if ( combination[ i ] ) {
       //loop through the vertex's span and count edges covered
       for ( let j = 0; j < combination.length; j++ ) {
-        //Do not check for loopbacks, accept if there is either a leading edge / a converging edge 
+        //Do not check for loopbacks, accept if there is either a leading edge / a converging edge
         //and if the covered set does not already have the edge
         if ( i !== j && ( network[ i ][ j ] === 1 || network[ j ][ i ] === 1 ) && !covered.has( `${j}${i}` ) ) {
           covered.add( `${i}${j}` );
@@ -51,11 +51,41 @@ const vertexCover = function ( combinations, combination ) {
   }
   return combinations;
 };
+//We need an independant set of one vertex at least
+let maxI = 1;
+const independantSet = function ( combinations, combination ) {
+  let count = 0;
+  let sum = 0;
+  //loop through the network
+  for ( let i = 0; i < network.length; i++ ) {
+    //calculate weight
+    sum += combination[ i ];
+    //init a secondary loop
+    for ( let j = 0; j < network.length; j++ ) {
+      //For each pass through an element, If the combination has the element set
+      //And If the combination has the secondary element set too
+      //And if there are no edges detected between the investigated element and
+      //the secondary element
+      //keep investigating
+      //If there is even a single edge, skip investigating
+      if ( i !== j && combination[ i ] && combination[ j ] && !count ) {
+        if ( network[ i ][ j ] || network[ j ][ i ] ) {
+          count++;
+        }
+      }
+    }
+  }
+  if ( !count && sum >= maxI ) {
+    maxI = sum;
+    combinations.add( combination );
+  }
+  return combinations;
+};
 //Define env
 const numberOfHubs = 5;
 const permutations = Math.pow( 2, numberOfHubs );
 //compute / generate combinations for number of permutations
-const possibilities = ( function () {
+const possibilities = function ( ascend = true ) {
   const combinations = [];
   for ( let i = permutations; i > 0; i-- ) {
     const combination = [];
@@ -68,11 +98,18 @@ const possibilities = ( function () {
       combination.unshift( state );
     }
     //push the current generated combination into the list of combinations
-    combinations.unshift( combination );
+    if ( ascend )
+      combinations.unshift( combination )
+    else
+      combinations.push( combination );
   }
   return combinations;
-} )();
+};
 //reduce the combinations generated for available permutations, to
 // a set of viable and minimum span combinations
-const combinations = possibilities.reduce( vertexCover, new Set() );
-console.log( possibilities, combinations );
+const combinationsOfIS = possibilities( false );
+const combinationsOfVC = possibilities();
+console.log( 'independant Set' );
+console.log( combinationsOfIS.reduce( independantSet, new Set() ) );
+console.log( 'Vertex Cover' );
+console.log( combinationsOfVC.reduce( vertexCover, new Set() ) );
