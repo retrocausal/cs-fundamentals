fed4963ecefb1e6268d7cc067088388a15aa7246
Nitin Prabhakar <pmitwithnitin@gmail.com>
2018-06-13 21:13:03 +0530

    Add a merge sort method  in JS

--- sort.js
+++ sort.js
@@ -25,10 +25,55 @@ class CustomSort {
     array[ index1 ] = temp;
     return array;
   }
+  naiveMerge( input ) {
+    const floor = 0;
+    const ceil = input.length;
+    const pivot = Math.floor( ( floor + ceil ) * ( 1 / 2 ) );
+    if ( ceil <= 1 )
+      return;
+    const A1 = input.slice( floor, pivot );
+    const A2 = input.slice( pivot, ceil );
+    this.naiveMerge( A1 );
+    this.naiveMerge( A2 );
+    return this.mergeHalves( input, A1, A2 );
+  }
+  mergeHalves( A, A1, A2 ) {
+    const merge = [];
+    let i = 0;
+    let j = 0;
+    while ( i < A1.length && j < A2.length ) {
+      if ( A1[ i ] < A2[ j ] ) {
+        merge.push( A1[ i ] );
+        i++;
+      } else {
+        merge.push( A2[ j ] );
+        j++;
+      }
+    }
+    while ( i < A1.length ) {
+      merge.push( A1[ i ] );
+      i++;
+    }
+    while ( j < A2.length ) {
+      merge.push( A2[ j ] );
+      j++;
+    }
+    let k = 0;
+    while ( k < A.length ) {
+      A[ k ] = ( merge[ k ] >= 0 || merge[ k ] < 0 ) ? merge[ k ] : A[ k ];
+      k++;
+    }
+    return A;
+  }
 }
-
+let time;
 const Sort = new CustomSort();
-const myList = [ 21, 4, 1, 3, 9, 20, 25, 6, 21, 14 ];
-const time = performance.now();
-console.log( Sort.bubble( myList ) );
+let myList = [ 986, 999, 1010, 23, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 21, 4, 1, 3, 9, 2334, 354, 1, 0, 0, 45, 123, 121, 111, 1111, 11111, 2324343434, 20, 25, 6, 21, 14 ];
+
+time = performance.now();
+console.log( Sort.bubble( myList.slice( 0 ) ) );
+console.log( performance.now() - time );
+
+time = performance.now();
+console.log( Sort.naiveMerge( myList.slice( 0 ) ) );
 console.log( performance.now() - time );
