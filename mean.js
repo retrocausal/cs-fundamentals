4145ce0ff65b43d0a66f7388205574d7f5c634df
Nitin Prabhakar <pmitwithnitin@gmail.com>
2018-06-28 12:34:31 +0530

    complete Mean at any point in time  calculator

--- mean.js
+++ mean.js
@@ -2,9 +2,11 @@ class Mean {
   constructor() {
     this.struct = [];
     this.mean = null;
+    this.sum = null;
   }
   add( value ) {
     this.struct.push( value );
+    return this.updateOnAdd( value );
   }
   remove( needle ) {
     let floor = 0;
@@ -29,16 +31,46 @@ class Mean {
       } );
       this.struct.length = 0;
       this.struct = newStruct;
+      return this.updateOnRemove( needle );
     }
+    return false;
   }
-  update( value ) {
-    if ( this.mean ) {
-
+  calc() {
+    let length = this.struct.length;
+    let sum = null;
+    if ( length ) {
+      sum = this.struct.reduce( ( total, element ) => {
+        total += element;
+        return total;
+      }, 0 );
     }
+    return sum;
+  }
+  updateOnAdd( value ) {
+    this.sum = ( this.sum === null ) ? this.calc() : this.sum + value;
+    this.mean = this.sum / this.struct.length;
+  }
+  updateOnRemove( value ) {
+    this.sum = ( this.sum === null ) ? this.calc() : this.sum - value;
+    if ( this.struct.length )
+      this.mean = this.sum / this.struct.length;
+    else
+      this.mean = 0;
+  }
+  getMean() {
+    return this.mean;
   }
 }
 
 const M = new Mean();
 for ( let i = 1; i <= 10; i++ ) {
   M.add( i );
+  console.log( M.getMean() );
 }
+console.log( M );
+for ( let i = 0; i < M.struct.length; i++ ) {
+  const element = M.struct[ i ];
+  M.remove( element );
+  console.log( M.getMean() );
+}
+console.log( M );
